3122253e470a5148d1cb10bad2a098ebfd0deaa1
Ricardo Fukui <r.fukui@gmail.com>
2014-05-13 06:08:58 -0300

    not working

--- agenda/models.py
+++ agenda/models.py
@@ -6,7 +6,7 @@ class Event(models.Model):
 	name = models.CharField(max_length = 50)
 	short_description = models.CharField(max_length = 150)
 	full_description = models.CharField(max_length = 1500)
-	image = models.CharField(max_length = 150)
+	image = models.FileField(upload_to='documents/%Y/%m/%d')
 	create_date = models.DateTimeField('Date Published')
 	last_alter = models.DateTimeField('last alteratio date')
 	last_alter_user = models.CharField(max_length = 50)
